c724d169df79f2097b93bdefdbe84848075ef075
KuritaAM <kurita.pm@gmail.com>
2023-10-28 14:39:19 +0900

    マージ後の調整

---
+++ PumpkinGame/Assets/Kurita/UI/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private float _frame = 0;
+
+    private void Update()
+    {
+        _frame += Time.deltaTime;
+
+        if (_frame > 2)
+        {
+            _frame = 0;
+            Debug.Log("ƒV[ƒ“‚ğ“Ç‚İ‚İ‚Ü‚·");
+            //SceneManager.LoadScene("");
+        }
+    }
+}
